86c3492c8ff4566867a9fd02df9c35870c7dc593
IsmailELY <ismail1999elyousfi@gmail.com>
2023-01-12 19:47:31 +0100

    Early_Commits_01.00.01

--- API.py
+++ API.py
@@ -4,20 +4,25 @@ import re
 
 class EntsogAPI:
     def __init__(self, uri:str=None) -> None:
-        self.set_uri(uri)
+        if uri: self.set_uri(uri)
+        self.r = requests.get("https://transparency.entsog.eu/api/v1")
 
     def set_uri(self, uri:str) -> None:
-        """Set up a correct Entsog API Uri"""
+        """Sets up a correct Entsog API Uri"""
         reg = "^https://transparency\.entsog\.eu/api/v1/(operationaldatas|cmpUnsuccessfulRequests|cmpUnavailables|cmpAuctions|interruptions|AggregatedData|tariffssimulations|tariffsfulls|urgentmarketmessages|connectionpoints|operators|balancingzones|operatorpointdirections|Interconnections|aggregateInterconnections).*limit=[^-].*"
         if re.search(reg, uri):
             self.uri = uri
+            self.r = requests.get(self.uri)
         else:
             raise Exception("Entsog API Uri is not valid")
 
-    def display_data(self) -> str:
-        r = requests.get(self.uri)
-        return json.dumps(r.json(), indent=2)
+    def __str__(self) -> str:
+        """Returns API result"""
+        if self.r.status_code < 300:
+            return json.dumps(self.r.json(), indent=2)
+        else:
+            return f"Status Code: {self.r.status_code}"
 
 if __name__=='__main__':
-    r = EntsogAPI(uri="https://transparency.entsog.eu/api/v1/operationaldatas?indicator=Interruptible%20Available&limit=5")
-    print(r.display_data())
+    r = EntsogAPI()
+    print(r)
