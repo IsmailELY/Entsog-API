30e5395465c5e0c1fd17c343bb7c4b99571e3513
IsmailELY <ismail1999elyousfi@gmail.com>
2023-01-15 00:16:54 +0100

    Early_Commits_01.00.06

--- OperationalAPI.py
+++ OperationalAPI.py
@@ -1,14 +1,19 @@
+from typing import Iterator
 import EntsogAPI
 
 class OperationalAPI(EntsogAPI):
     def __init__(self, url:str=None, **kwargs) -> None:
         super().__init__(url=url, **kwargs)
 
-    def read_json(self, offset=0, limit=1000) -> dict:
-        """Read API Content"""
-        self.set_payload(offset=offset, limit=limit)
-        content = self.__r.json()
-        if content:
-            return content["operationaldatas"] | self.read(offset=offset+limit, limit=limit)
-        return None
+    def read_json(self, offset:int=0, destination:int=1000, pace:int=1000) -> Iterator[dict]:
+        """Generator of Operational Data API Content"""
+        def load_batch(offset):
+            self.set_payload(offset=offset, limit=pace)
+            return self.__r.json()["operationaldatas"]
+        Iter = 0
+        while Iter < destination:
+            for record in load_batch(offset=Iter):
+                yield record
+                Iter += 1
+                if Iter == destination: break
 
