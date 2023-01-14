ab021dce5faf5fb2c25287347fe71b48ac57153c
IsmailELY <ismail1999elyousfi@gmail.com>
2023-01-14 23:39:15 +0100

    Early_Commits_01.00.05

--- OperationalAPI.py
+++ OperationalAPI.py
@@ -4,3 +4,11 @@ class OperationalAPI(EntsogAPI):
     def __init__(self, url:str=None, **kwargs) -> None:
         super().__init__(url=url, **kwargs)
 
+    def read_json(self, offset=0, limit=1000) -> dict:
+        """Read API Content"""
+        self.set_payload(offset=offset, limit=limit)
+        content = self.__r.json()
+        if content:
+            return content["operationaldatas"] | self.read(offset=offset+limit, limit=limit)
+        return None
+
