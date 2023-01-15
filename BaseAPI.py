083bb1d0bdd7b2dd1816636a5cf671d75f3d6353
IsmailELY <ismail1999elyousfi@gmail.com>
2023-01-15 12:32:49 +0100

    build: Adjust Base API and build API Iterator

--- BaseAPI.py
+++ BaseAPI.py
@@ -12,7 +12,7 @@ class BaseAPI:
         'connectionpoints','operators','balancingzones','operatorpointdirections','Interconnections',
         'aggregateInterconnections')
         self.__payload = {'offset':'0', 'limit':'5'}
-        self.build_uri(category=category)
+        self.set_uri(category=category)
         if kwargs: self.set_payload(**kwargs)
 
     def Reload(method):
@@ -21,10 +21,11 @@ class BaseAPI:
             method(self, *args, **kwargs)
             if self.get_uri(): 
                 self.__r = requests.get(self.get_uri(), params=self.__payload)
+                self._fields = self.__r.json()["meta"]["fields"]
                 self.count_req+=1
         return inner
 
-    def __set_uri(self, uri:str) -> None:
+    def __check_uri(self, uri:str) -> None:
         """Checks the compliance of the Entsog API Uri"""
         reg = "^https://transparency\.entsog\.eu/api/v1/(operationaldatas|cmpUnsuccessfulRequests|cmpUnavailables|cmpAuctions|interruptions|AggregatedData|tariffssimulations|tariffsfulls|urgentmarketmessages|connectionpoints|operators|balancingzones|operatorpointdirections|Interconnections|aggregateInterconnections)*"
         if re.search(reg, uri):
@@ -33,6 +34,14 @@ class BaseAPI:
         else:
             raise Exception("Entsog API Uri is not valid")
 
+    def __str__(self) -> str:
+        """Returns API result"""
+        if self.__r.status_code < 300:
+            return json.dumps(self.__r.json(), indent=2)
+        else:
+            return f"Status Code: {self.__r.status_code}"
+    
+    #Setters
     @Reload
     def set_payload(self, **kwargs) -> None:
         """Apply filters"""
@@ -41,32 +50,35 @@ class BaseAPI:
                 continue    
             self.__payload[property] = str(value)
 
-    def build_uri(self, category:str) -> None:
+    def set_uri(self, category:str) -> None:
         """Set up URI for the selected category"""
         if category in self.__categories:
             b_uri = self.__base_uri + category
-            self.__set_uri(uri=b_uri)
+            self.__check_uri(uri=b_uri)
         else:
             raise Exception(f"API category '{category}' does not exist.\nPlease select one of the following:\n{','.join(self.__categories)}")
     
+    #Getters
     def get_uri(self) -> str:
         """Get Curent API URI"""
-        if self.__uri: return self.__uri
-        return None
+        try:
+            return self.__uri
+        except AttributeError:
+            raise Exception("Url is not configured yet.")
 
-    def __str__(self) -> str:
-        """Returns API result"""
-        if self.__r.status_code < 300:
-            return json.dumps(self.__r.json(), indent=2)
-        else:
-            return f"Status Code: {self.__r.status_code}"
-
+    def _get_request(self) -> object:
+        """Get Current request object"""
+        try:
+            return self.__r
+        except AttributeError:
+            raise Exception("No request have been issued yet.")
+    #Abstract methods
     def read_json(self):
         pass
 
 
 if __name__=='__main__':
-    d = BaseAPI('operationaldatas', indicator='Interruptible Available', directionKey='exit', tsoItemIdentifier='21Z000000OGE0154', limit=-1)
-    #d = BaseAPI()
-    #d.set_payload(indicator='Interruptible Available', directionKey='exit')
-    print(d)
+    #d = BaseAPI('operationaldatas', indicator='Interruptible Available', directionKey='exit', tsoItemIdentifier='21Z000000OGE0154', limit=-1)
+    d = BaseAPI()
+    d.set_payload(offset=0, limit=1000, indicator='Interruptible Available', directionKey='exit')
+    print(d.get_uri())
--- CategorizedAPI.py
+++ CategorizedAPI.py
@@ -1,15 +1,16 @@
-from typing import Iterator
 import BaseAPI
+from typing import Iterator
+import time
 
-class OperationalAPI(BaseAPI):
-    def __init__(self, url:str=None, **kwargs) -> None:
-        super().__init__(url=url, **kwargs)
+class OperationalAPI(BaseAPI.BaseAPI):
+    def __init__(self, **kwargs) -> None:
+        super().__init__(category="operationaldatas", **kwargs)
 
     def read_json(self, offset:int=0, destination:int=1000, pace:int=1000) -> Iterator[dict]:
         """Generator of Operational Data API Content"""
         def load_batch(offset):
             self.set_payload(offset=offset, limit=pace)
-            return self.__r.json()["operationaldatas"]
+            return self._get_request().json()["operationaldatas"]
         Iter = 0
         while Iter < destination:
             for record in load_batch(offset=Iter):
@@ -18,5 +19,15 @@ class OperationalAPI(BaseAPI):
                 if Iter == destination: break
 
     def set_payload(self, **kwargs) -> None:
-        """Redefine"""
-        super().set_payload(self, **kwargs)
+        if hasattr(self, '_fields'):
+            if kwargs.keys() in self._fields: raise Exception(f"""The following keys: {', '.join([key for key in kwargs.keys() if key not in self._fields])} do not exist.
+            Please use the available keys for the current API: {', '.join([iter for iter in self._fields])}""") 
+        super().set_payload(**kwargs)
+
+
+
+if __name__=='__main__':
+    a = OperationalAPI()
+    for row in a.read_json(0,50):
+        print("row: ",row)
+        time.sleep(0.3)
